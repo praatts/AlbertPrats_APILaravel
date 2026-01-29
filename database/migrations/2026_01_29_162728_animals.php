bdfd75d592f0402064b4610a1239b5b8e57e1a37
unknown <albertprba@alumnes.ilerna.com>
2026-01-29 18:38:08 +0100

    Migrations

---
+++ database/migrations/2026_01_29_162728_animals.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id('id_animal');
+            $table->string('nombre')->nullable(false);
+            $table->enum('tipo', ['perro', 'gato', 'conejo', 'hámster'])->nullable(false);
+            $table->double('peso');
+            $table->string('enfermedad');
+            $table->text('comentarios');
+            $table->foreignId('owner_id')->constrained('owners', 'id_persona')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('animals');
+    }
+};
